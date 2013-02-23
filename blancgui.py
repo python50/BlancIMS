670ea6d617d1d46009c375ce8b747f3f11e62787
Jason White <jason@jswhite.net>
2013-02-23 18:05:53 -0500

    ok

--- blancgui.py
+++ blancgui.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 
+"""
+	BlancIMS - Open Source Inventory Management
+	http://github.com/python50/BlancIMS
+	
+	The goal of BlancIMS is to create a readily available, easy to use 
+	solution for small inventory management task. In specific it was created
+	for use by Jason White during the Gannon Programming Contest to keep
+	track of his rather large collection electronic components he has
+	amassed for his hobbies of electronics and robotics.
+	
+	BlancIMS Copyright (C) 2013 Jason White
+	This project is licensed under GPLv2.
+"""
+
 import os
 import datetime
 import pygtk
